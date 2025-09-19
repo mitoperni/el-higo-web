e8e108325a2c121a35149b26f82dc9ec847e0d37
mitoperni <mitoperni@gmail.com>
2025-09-19 09:20:27 +0200

    Images changed to match products

--- src/components/features/Menu.jsx
+++ src/components/features/Menu.jsx
@@ -5,53 +5,54 @@ const Menu = () => {
 
   const menuCategories = [
     {
-      id: 'portions',
-      name: t('categories.portions', { ns: 'menu' }),
-      items: t('items.portions', { returnObjects: true, ns: 'menu' }) || [],
+      id: "starters",
+      name: t("categories.starters", { ns: "menu" }),
+      items: t("items.starters", { returnObjects: true, ns: "menu" }) || [],
       images: [
-        'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?w=300&h=200&fit=crop', // aguacate con langostinos - avocado dish with shrimp
-        'https://images.unsplash.com/photo-1571091655789-405eb7a3a3a8?w=300&h=200&fit=crop', // hummus - traditional hummus with pita
-        'https://images.unsplash.com/photo-1486297678162-eb2a19b0a32d?w=300&h=200&fit=crop', // queso idiazábal - Spanish sheep cheese
-        'https://images.unsplash.com/photo-1549888834-3ec93abae044?w=300&h=200&fit=crop', // jamón de Trevélez - Spanish cured ham
-        'https://images.unsplash.com/photo-1579952363873-27d3bfad9c0d?w=300&h=200&fit=crop', // tataki de atún - seared tuna tataki
-        'https://images.unsplash.com/photo-1544975503-e1462e3466c0?w=300&h=200&fit=crop', // tabla de patés veganos - vegan pâté board
-        'https://images.unsplash.com/photo-1547592166-23ac45744acd?w=300&h=200&fit=crop'  // crema del día - soup of the day
-      ]
+        "https://www.gallinablanca.es/wp-content/uploads/2006/04/15401139853c45e78482f6a489865cab8522b47b6e.jpg", // aguacate con langostinos - avocado dish with shrimp
+        "https://www.daisybrand.com/wp-content/uploads/2019/12/creamy-hummus-dip2-770x628_3571.jpg", // hummus - traditional hummus with pita
+        "https://i0.wp.com/bardonairecomidasadomicilio.com/wp-content/uploads/2020/09/queso-racion-media-original.jpg?fit=1400%2C1050&ssl=1", // queso idiazábal - Spanish sheep cheese
+        "https://www.jamonesvallejo.com/wp-content/uploads/2021/05/Diferencias-entre-el-jamon-de-Trevelez-y-jamon-de-la-Alpujarra.jpg", // jamón de Trevélez - Spanish cured ham
+        "https://www.pescadoscharo.es/wp-content/uploads/2018/05/tataki-atun.jpeg", // tataki de atún - seared tuna tataki
+        "https://cdn.recipes.lidl/images/_Aliases/es-ES/recipe_detail_1264x494/336bf0d2eb14-banner-pates-veganos.jpg", // tabla de patés veganos - vegan pâté board
+        "https://imag.bonviveur.com/emplatado-final-del-pure-de-verduras.jpg", // crema del día - soup of the day
+      ],
     },
     {
-      id: 'sandwiches',
-      name: t('categories.sandwiches', { ns: 'menu' }),
-      items: t('items.sandwiches', { returnObjects: true, ns: 'menu' }) || [],
+      id: "sandwiches",
+      name: t("categories.sandwiches", { ns: "menu" }),
+      items: t("items.sandwiches", { returnObjects: true, ns: "menu" }) || [],
       images: [
-        'https://images.unsplash.com/photo-1619096252214-ef06c45683e3?w=300&h=200&fit=crop', // bocadillo de salmón - salmon sandwich
-        'https://images.unsplash.com/photo-1540420773420-3366772f4999?w=300&h=200&fit=crop', // bocadillo vegetal - veggie sandwich
-        'https://images.unsplash.com/photo-1520072959219-c595dc870360?w=300&h=200&fit=crop', // hamburguesa vegana - vegan burger
-        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=300&h=200&fit=crop'  // hamburguesa ternera - beef burger
-      ]
+        "https://www.hola.com/horizon/landscape/54f537cc23ab-bocadillo-gourmet-salmon-compota-tomate-t.jpg", // bocadillo de salmón - salmon sandwich
+        "https://img2.rtve.es/v/5170242?w=1600&preview=1556641967805.png", // bocadillo vegetal - veggie sandwich
+        "https://img2.rtve.es/n/2119800", // hamburguesa vegana - vegan burger
+        "https://www.carniceriademadrid.es/wp-content/uploads/2024/12/goat-cheese-and-caramelized-onion-burger-2023-11-27-05-00-17-utc-scaled.jpg", // hamburguesa ternera - beef burger
+      ],
     },
     {
-      id: 'salads',
-      name: t('categories.salads', { ns: 'menu' }),
-      items: t('items.salads', { returnObjects: true, ns: 'menu' }) || [],
+      id: "salads",
+      name: t("categories.salads", { ns: "menu" }),
+      items: t("items.salads", { returnObjects: true, ns: "menu" }) || [],
       images: [
-        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=300&h=200&fit=crop', // ensalada El Higo (mixta con frutas) - mixed salad with fruits
-        'https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?w=300&h=200&fit=crop', // espinacas y pavo - spinach and turkey salad
-        'https://images.unsplash.com/photo-1546793665-c74683f339c1?w=300&h=200&fit=crop', // ensalada de ahumados - smoked fish salad
-        'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?w=300&h=200&fit=crop'  // caprese di buffala - caprese with buffalo mozzarella
-      ]
+        "https://i.ytimg.com/vi/hh0uzCsIzDo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBroivJpcKxlpAsnZQ3taUSIlhyug", // ensalada El Higo (mixta con frutas) - mixed salad with fruits
+        "https://static.bainet.es/clip/bcf91562-8212-41dd-af02-96ec65a7f91a_source-aspect-ratio_1600w_0.jpg", // espinacas y pavo - spinach and turkey salad
+        "https://www.gastronomiaycia.com/wp-content/uploads/2015/11/ensalada_ahumados_frutos1p.jpg", // ensalada de ahumados - smoked fish salad
+        "https://imag.bonviveur.com/ensalada-de-burrata-con-tomate-y-pesto-abierta.jpg", // caprese di buffala - caprese with buffalo mozzarella
+      ],
     },
     {
-      id: 'meat_and_fish',
-      name: t('categories.meat_and_fish', { ns: 'menu' }),
-      items: t('items.meat_and_fish', { returnObjects: true, ns: 'menu' }) || [],
+      id: "meat_and_fish",
+      name: t("categories.meat_and_fish", { ns: "menu" }),
+      items:
+        t("items.meat_and_fish", { returnObjects: true, ns: "menu" }) || [],
       images: [
-        'https://images.unsplash.com/photo-1579952363873-27d3bfad9c0d?w=300&h=200&fit=crop', // atún rojo - red tuna with vegetables
-        'https://images.unsplash.com/photo-1559562589-d9bf87717ccf?w=300&h=200&fit=crop', // bacalao gratinado - gratinated cod
-        'https://images.unsplash.com/photo-1544025162-d76694265947?w=300&h=200&fit=crop', // entraña de ternera - beef skirt steak
-        'https://images.unsplash.com/photo-1574894709920-11b28e7367e3?w=300&h=200&fit=crop', // carrillada de ternera - beef cheek in sauce
-        'https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?w=300&h=200&fit=crop'  // plato infantil (pavo con papas) - children's turkey with potatoes
-      ]
-    }
+        "https://okdiario.com/img/2018/11/14/receta-de-wok-de-atun-655x368.jpg", // atún rojo - red tuna with vegetables
+        "https://content-cocina.lecturas.com/medio/2024/10/15/wok-de-arroz-integral-con-verduras-y-bacalao_00000000_241030153503_1200x1200.jpg", // bacalao gratinado - gratinated cod
+        "https://www.carnescarrasquilla.es/wp-content/uploads/2022/04/Entrana-de-ternera-que-es-y-como-prepararla-Carnes-Carrasquilla.jpg", // entraña de ternera - beef skirt steak
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjMrSgusquJoIduP9IxCKgHds5gYsDGumINMd6__yLJ53dUlL42ehTA8dHiklUm7fEa_djlgRgUauXd4w-cxBaRB7alcFkdVwv4fDdGtiSrIVlTVBO3N_aQ_N9OvqI0f2mWUvs1SS7FlS0/s1600/DSCN2471.JPG", // carrillada de ternera - beef cheek in sauce
+        "https://media-cdn.tripadvisor.com/media/photo-s/07/ea/2a/71/pavo-a-la-plancha-con.jpg", // plato infantil (pavo con papas) - children's turkey with potatoes
+      ],
+    },
   ];
 
   return (
